0c3e68d7b6be18c72d5ebac1b2fa239b62ff684a
HARVINDER-FSD <hs8339952@gmail.com>
2024-08-05 11:14:47 +0530

    jq data

--- tasks/2image.js
+++ tasks/2image.js
@@ -1,70 +1,89 @@
 
 $(document).ready(function () {
-    $("button").click(function () {
-      $(".album1").css({
+    $("#but").click(function () {
+      $(".album1").animate({
         top: "0",
         left: "0",
-      });
-      $(".album2").css({
+      },2000);
+      $(".album2").animate({
         top: "0px",
         left: "250px",
-      });
-      $(".album3").css({
+      },2000);
+      $(".album3").animate({
         top: "0px",
         left: "500px",
-      });
-      $(".album4").css({
+      },2000);
+      $(".album4").animate({
         top: "0px",
         left: "750px",
-      });
-      $(".album5").css({
+      },2000);
+      $(".album5").animate({
         top: "0px",
         left: "1000px",
-      });
-      $(".album6").css({
+      },2000);
+      $(".album6").animate({
         top: "0px",
         left: "1200px",
-      });
-      $(".album7").css({
+      },2000);
+      $(".album7").animate({
         top: "0px",
         left: "1400px",
-      });
-      $(".album8").css({
+      },2000);
+      $(".album8").animate({
         top: "0px",
         left: "1650px",
-      });
-      $(".album9").css({
+      },2000);
+      $(".album9").animate({
         top: "0",
         left: "0",
-      });
-      $(".album10").css({
+      },3000);
+      $(".album10").animate({
         top: "0px",
-        left: "1850px",
-      });
-      $(".album11").css({
+        left: "250px",
+      },3000);
+      $(".album11").animate({
         top: "0px",
-        left: "2000px",
-      });
-      $(".album12").css({
+        left: "500px",
+      },3000);
+      $(".album12").animate({
         top: "0px",
-        left: "2150px",
-      });
-      $(".album13").css({
+        left: "750px",
+      },3000);
+      $(".album13").animate({
         top: "0px",
-        left: "2300px",
-      });
-      $(".album14").css({
+        left: "1000px",
+      },3000);
+      $(".album14").animate({
         top: "0px",
-        left: "2500px",
-      });
-      $(".album15").css({
+        left: "1200px",
+      },3000);
+      $(".album15").animate({
         top: "0px",
-        left: "2800px",
-      });
-      $(".album16").css({
+        left: "1400px",
+      },3000);
+      $(".album16").animate({
         top: "0px",
-        left: "3050px",
-      });
+        left: "1650px",
+      },3000);
     });
+    $("#left").click(function(){
+     $(".container").animate({
+      top:"0",
+      left:"500px"
+     },2000)
+    })
+    $("#right").click(function(){
+      $(".container").animate({
+       top:"0",
+       right:"500px"
+      },2000)
+     })
+     $("#reset").click(function(){
+      $(".container").animate({
+       top:"0",
+       left:"0"
+
+      })
+     })
   });
 
